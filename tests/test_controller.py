e3f332a98f5ac04a0942f9be1eb90858a9e4b52e
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-25 09:17:14 +0200

    First step- Run method counter deleted. If condition in the mock view deleted. The values to get_user_input passed directly from test case function. User_input_database table was ndeclared in the class MockView. In each each case the values or the data needed for the test were filled in the table and used for the respected test case. Second Step -

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -6,7 +6,9 @@ from calculate import view
 class MockView:
     get_user_input_counter = 0
     print_result_call = []
-    run_method_counter = 0
+    # run_method_counter = 0
+    user_input_database = []
+    #user_input_database = [0,("1","42+100", 142.0),("1","42+100+90", 232.0),("2","100-60", 40.0),("3","2*3", 6),("4","6.0 / 2.0", 3.0)]
 
     @staticmethod
     def print_menu():
@@ -19,35 +21,47 @@ class MockView:
         # 42+100 
         # 5 pour quitter
         
-        MockView.get_user_input_counter += 1
-        
-        if  MockView.run_method_counter == 1 :
-            if MockView.get_user_input_counter == 1:
-                return "1"
-            
-            if MockView.get_user_input_counter == 2:
-                return "42+100"
-        
-        if  MockView.run_method_counter == 2 :
-            if MockView.get_user_input_counter == 1:
-                return "2"
-            
-            if MockView.get_user_input_counter == 2:
-                return "100-60"
+        # MockView.get_user_input_counter += 1
+        # return MockView.user_input_database[MockView.get_user_input_counter - 1]
 
-        if  MockView.run_method_counter == 3 :
-            if MockView.get_user_input_counter == 1:
-                return "3"
+        value = MockView.user_input_database[MockView.get_user_input_counter]
+        MockView.get_user_input_counter += 1
+        return value
+        
+        #if  MockView.run_method_counter == MockView.user_input_database[ MockView.run_method_counter][0]:
+        # if MockView.get_user_input_counter == 1:
+        #     return MockView.user_input_database[0]
+        
+        # if MockView.get_user_input_counter == 2:
+        #     return MockView.user_input_database[1]
+        
+        # if  MockView.run_method_counter == 1 :
+        #     if MockView.get_user_input_counter == 1:
+        #         return "1"
             
-            if MockView.get_user_input_counter == 2:
-                return "2*3"
+        #     if MockView.get_user_input_counter == 2:
+        #         return "42+100"
+        
+        # if  MockView.run_method_counter == 2 :
+        #     if MockView.get_user_input_counter == 1:
+        #         return "2"
             
-        if  MockView.run_method_counter == 4 :
-            if MockView.get_user_input_counter == 1:
-                return "4"
+        #     if MockView.get_user_input_counter == 2:
+        #         return "100-60"
+
+        # if  MockView.run_method_counter == 3 :
+        #     if MockView.get_user_input_counter == 1:
+        #         return "3"
             
-            if MockView.get_user_input_counter == 2:
-                return "6.0 / 2.0"
+        #     if MockView.get_user_input_counter == 2:
+        #         return "2*3"
+            
+        # if  MockView.run_method_counter == 4 :
+        #     if MockView.get_user_input_counter == 1:
+        #         return "4"
+            
+        #     if MockView.get_user_input_counter == 2:
+        #         return "6.0 / 2.0"
         return "5"
 
     @staticmethod
@@ -76,44 +90,69 @@ def setup_mock(mocker):
 
 def test_run_add(mocker):
     sut = Controller()
-    MockView.run_method_counter = 1
+    MockView.user_input_database = ("1", "100+40", "5")
 
     setup_mock(mocker)
 
     sut.run()
 
     assert len(MockView.print_result_call) == 1
-    assert MockView.print_result_call[0] == ("42+100", 142.0)
+    assert MockView.print_result_call[0] == ("100+40", 140)
 
-def test_run_sub(mocker):
+def test_run_add_bis(mocker):
     sut = Controller()
-    MockView.run_method_counter = 2
-    
+    MockView.user_input_database = ("1", "100+40+50","5")
+
     setup_mock(mocker)
 
     sut.run()
-    
-    assert len(MockView.print_result_call) == 1
-    assert MockView.print_result_call[0] == ("100-60", 40.0)
 
-def test_run_multi(mocker):
+    assert len(MockView.print_result_call) == 1
+    assert MockView.print_result_call[0] == ("100+40+50", 190)
+
+def test_run_add_bis_another(mocker):
     sut = Controller()
-    MockView.run_method_counter = 3
-    
+    MockView.user_input_database = ("1", "100+40+50", "1", "1+2+3+4+5", "5")
+
     setup_mock(mocker)
 
     sut.run()
-    
-    assert len(MockView.print_result_call) == 1
-    assert MockView.print_result_call[0] == ("2*3", 6)
 
-def test_run_div(mocker):
-    sut = Controller()
-    MockView.run_method_counter = 4
-    
-    setup_mock(mocker)
+    assert len(MockView.print_result_call) == 2
+    assert MockView.print_result_call[0] == ("100+40+50", 190)
+    assert MockView.print_result_call[1] == ("1+2+3+4+5", 15)
 
-    sut.run()
+
+
+# def test_run_sub(mocker):
+#     sut = Controller()
+#     MockView.run_method_counter = 2
     
-    assert len(MockView.print_result_call) == 1
-    assert MockView.print_result_call[0] == ("6.0 / 2.0", 3.0)
+#     setup_mock(mocker)
+
+#     sut.run()
+    
+#     assert len(MockView.print_result_call) == 1
+#     assert MockView.print_result_call[0] == ("100-60", 40.0)
+
+# def test_run_multi(mocker):
+#     sut = Controller()
+#     MockView.run_method_counter = 3
+    
+#     setup_mock(mocker)
+
+#     sut.run()
+    
+#     assert len(MockView.print_result_call) == 1
+#     assert MockView.print_result_call[0] == ("2*3", 6)
+
+# def test_run_div(mocker):
+#     sut = Controller()
+#     MockView.run_method_counter = 4
+    
+#     setup_mock(mocker)
+
+#     sut.run()
+    
+#     assert len(MockView.print_result_call) == 1
+#     assert MockView.print_result_call[0] == ("6.0 / 2.0", 3.0)
