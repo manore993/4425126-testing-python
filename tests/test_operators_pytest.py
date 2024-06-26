ea00c01f4a8b4346c4344f88230d9cfb98a92dce
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-26 18:23:36 +0200

    Parametrization of the first test case. (One test case that has two variables (input and output) - 6 different types of input-output pairs were tried.

--- tests/test_operators_pytest.py
+++ tests/test_operators_pytest.py
@@ -1,10 +1,21 @@
 from calculate.operators import Operators
+import pytest
 
-def test_addition():
+@pytest.mark.parametrize("addition, expected_result", [
+("3+5", 8),
+("5+9", 14),
+("8+2", 10),
+("4+98", 102),
+("90", 90),
+("1+2+3+4+5", 15)
+])
+
+def test_addition(addition, expected_result):
     sut = Operators()
-    operation = "5.5 + 10 + 30 + 13.7"
-    expected_value = 59.2
-    assert sut.addition(operation) == expected_value
+    # operation = "5.5 + 10 + 30 + 13.7"
+    # expected_value = 59.2
+    # assert sut.addition(operation) == expected_value
+    assert sut.addition(addition) == expected_result
 
 def test_substraction():
     sut = Operators()
