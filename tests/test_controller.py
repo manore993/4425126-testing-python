4bebf5670b1200bb3821885a0471a07ee7b929a3
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-26 10:54:17 +0200

    Operator Class replaced by its mock MockOperators. For each operation idividual user_output_database was created inside functions dedicated to individual operation. (expected user outputs to compare the results with during testing). When functions are called, the values inside these database are taken wrt the index (which is its own trigger counter for example addition_counter).

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,14 +1,51 @@
 from calculate.controller import Controller
 from calculate.view import View
-from calculate import view
+from calculate.operators import Operators
 
+class MockOperators:
+    user_output_database = []  
+    addition_counter = 0
+    add_output_database = []
+    substraction_counter = 0
+    sub_output_database = []
+    multiplication_counter = 0
+    multi_output_database = []
+    division_counter = 0
+    div_output_database = []
+
+    @staticmethod
+    def addition(self, operation):
+        
+        value = MockOperators.add_output_database[MockOperators.addition_counter]
+        MockOperators.addition_counter += 1 
+        return value
+    
+    @staticmethod
+    def substraction(self, operation):
+
+        value = MockOperators.sub_output_database[MockOperators.substraction_counter]
+        MockOperators.substraction_counter += 1
+        return value
+    
+    @staticmethod
+    def multiplication(self, operation):
+
+        value = MockOperators.multi_output_database[MockOperators.multiplication_counter]
+        MockOperators.multiplication_counter += 1
+        return value
+    
+    @staticmethod
+    def division(self, operation):
+        print(MockOperators.division_counter)
+        value = MockOperators.div_output_database[MockOperators.division_counter]
+        MockOperators.division_counter += 1
+        print(value)
+        return value
 
 class MockView:
     get_user_input_counter = 0
     print_result_call = []
-    # run_method_counter = 0
     user_input_database = []
-    #user_input_database = [0,("1","42+100", 142.0),("1","42+100+90", 232.0),("2","100-60", 40.0),("3","2*3", 6),("4","6.0 / 2.0", 3.0)]
 
     @staticmethod
     def print_menu():
@@ -16,54 +53,12 @@ class MockView:
 
     @staticmethod
     def get_user_input(input_msg):        
-        # On veut faire 
-        # 1 -> addition
-        # 42+100 
-        # 5 pour quitter
-        
-        # MockView.get_user_input_counter += 1
-        # return MockView.user_input_database[MockView.get_user_input_counter - 1]
 
         value = MockView.user_input_database[MockView.get_user_input_counter]
         MockView.get_user_input_counter += 1
         return value
         
-        #if  MockView.run_method_counter == MockView.user_input_database[ MockView.run_method_counter][0]:
-        # if MockView.get_user_input_counter == 1:
-        #     return MockView.user_input_database[0]
-        
-        # if MockView.get_user_input_counter == 2:
-        #     return MockView.user_input_database[1]
-        
-        # if  MockView.run_method_counter == 1 :
-        #     if MockView.get_user_input_counter == 1:
-        #         return "1"
-            
-        #     if MockView.get_user_input_counter == 2:
-        #         return "42+100"
-        
-        # if  MockView.run_method_counter == 2 :
-        #     if MockView.get_user_input_counter == 1:
-        #         return "2"
-            
-        #     if MockView.get_user_input_counter == 2:
-        #         return "100-60"
-
-        # if  MockView.run_method_counter == 3 :
-        #     if MockView.get_user_input_counter == 1:
-        #         return "3"
-            
-        #     if MockView.get_user_input_counter == 2:
-        #         return "2*3"
-            
-        # if  MockView.run_method_counter == 4 :
-        #     if MockView.get_user_input_counter == 1:
-        #         return "4"
-            
-        #     if MockView.get_user_input_counter == 2:
-        #         return "6.0 / 2.0"
-        return "5"
-
+       
     @staticmethod
     def end_message(): 
         pass
@@ -79,18 +74,26 @@ class MockView:
 def setup_mock(mocker):
     MockView.print_result_call = []
     MockView.get_user_input_counter = 0
+    MockOperators.addition_counter = 0
+    MockOperators.substraction_counter = 0
+    MockOperators.multiplication_counter = 0
+    MockOperators.division_counter = 0
 
     mocker.patch.object(View, 'print_menu', MockView.print_menu)
     mocker.patch.object(View, 'get_user_input', MockView.get_user_input)
     mocker.patch.object(View, 'end_message', MockView.end_message)
     mocker.patch.object(View, 'continue_message', MockView.continue_message)
     mocker.patch.object(View, 'print_result', MockView.print_result)
-
+    mocker.patch.object(Operators, 'addition', MockOperators.addition)
+    mocker.patch.object(Operators, 'substraction', MockOperators.substraction)
+    mocker.patch.object(Operators, 'multiplication', MockOperators.multiplication)
+    mocker.patch.object(Operators, 'division', MockOperators.division)
 
 
 def test_run_add(mocker):
     sut = Controller()
     MockView.user_input_database = ("1", "100+40", "5")
+    MockOperators.add_output_database = [140]
 
     setup_mock(mocker)
 
@@ -102,7 +105,8 @@ def test_run_add(mocker):
 def test_run_add_bis(mocker):
     sut = Controller()
     MockView.user_input_database = ("1", "100+40+50","5")
-
+    MockOperators.add_output_database = [190]
+    
     setup_mock(mocker)
 
     sut.run()
@@ -113,7 +117,8 @@ def test_run_add_bis(mocker):
 def test_run_add_bis_another(mocker):
     sut = Controller()
     MockView.user_input_database = ("1", "100+40+50", "1", "1+2+3+4+5", "5")
-
+    MockOperators.add_output_database = [190, 15]
+    
     setup_mock(mocker)
 
     sut.run()
@@ -125,7 +130,8 @@ def test_run_add_bis_another(mocker):
 def test_run_sub(mocker):
     sut = Controller()
     MockView.user_input_database = ("2", "100-60", "5")
-    
+    MockOperators.sub_output_database = [40]
+
     setup_mock(mocker)
 
     sut.run()
@@ -135,18 +141,21 @@ def test_run_sub(mocker):
 
 def test_run_multi(mocker):
     sut = Controller()
-    MockView.user_input_database = ("3", "2*3", "5")
+    MockView.user_input_database = ("3", "2*3","3", "4*6", "5")
+    MockOperators.multi_output_database  = [6, 24]
     
     setup_mock(mocker)
 
     sut.run()
     
-    assert len(MockView.print_result_call) == 1
-    assert MockView.print_result_call[0] == ("2*3", 6)
+    assert len(MockView.print_result_call) == 2
+    assert MockView.print_result_call[0] == ("2*3", 6) 
+    assert MockView.print_result_call[1] == ("4*6", 24)
 
 def test_run_div(mocker):
     sut = Controller()
     MockView.user_input_database = ("4", "6.0 / 2.0", "4", "6.0 / 0.0", "5")
+    MockOperators.div_output_database  = [3.0, None]
     
     setup_mock(mocker)
 
@@ -155,3 +164,19 @@ def test_run_div(mocker):
     assert len(MockView.print_result_call) == 2
     assert MockView.print_result_call[0] == ("6.0 / 2.0", 3.0)
     assert MockView.print_result_call[1] == ("6.0 / 0.0", None)
+
+def test_run_allfunctions(mocker):
+    sut = Controller()
+    MockView.user_input_database = ("3", "2*3","3", "4*6","4", "6.0 / 2.0", "4", "6.0 / 0.0", "5")
+    MockOperators.multi_output_database = [6, 24]
+    MockOperators.div_output_database = [3.0, None]
+    
+    setup_mock(mocker)
+
+    sut.run()
+    
+    assert len(MockView.print_result_call) == 4
+    assert MockView.print_result_call[0] == ("2*3", 6) 
+    assert MockView.print_result_call[1] == ("4*6", 24)
+    assert MockView.print_result_call[2] == ("6.0 / 2.0", 3.0)
+    assert MockView.print_result_call[3] == ("6.0 / 0.0", None)
