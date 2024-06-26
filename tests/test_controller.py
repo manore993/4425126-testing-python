5cd52beeba46b040881e7a1669390de04c3848c6
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-26 15:54:56 +0200

    Function set_up replaced by a fixture, because this function includes steps to be executed in every test case.

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,6 +1,9 @@
 from calculate.controller import Controller
 from calculate.view import View
 from calculate.operators import Operators
+import pytest
+
+
 
 class MockOperators:
     user_output_database = []  
@@ -71,6 +74,7 @@ class MockView:
     def print_result(operation, result):
         MockView.print_result_call += [(operation, result)]
         
+@pytest.fixture
 def setup_mock(mocker):
     MockView.print_result_call = []
     MockView.get_user_input_counter = 0
@@ -87,39 +91,51 @@ def setup_mock(mocker):
     mocker.patch.object(Operators, 'addition', MockOperators.addition)
     mocker.patch.object(Operators, 'substraction', MockOperators.substraction)
     mocker.patch.object(Operators, 'multiplication', MockOperators.multiplication)
-    mocker.patch.object(Operators, 'division', MockOperators.division)
+    mocker.patch.object(Operators, 'division', MockOperators.division)    
+
+# def setup_mock(mocker):
+#     MockView.print_result_call = []
+#     MockView.get_user_input_counter = 0
+#     MockOperators.addition_counter = 0
+#     MockOperators.substraction_counter = 0
+#     MockOperators.multiplication_counter = 0
+#     MockOperators.division_counter = 0
+
+#     mocker.patch.object(View, 'print_menu', MockView.print_menu)
+#     mocker.patch.object(View, 'get_user_input', MockView.get_user_input)
+#     mocker.patch.object(View, 'end_message', MockView.end_message)
+#     mocker.patch.object(View, 'continue_message', MockView.continue_message)
+#     mocker.patch.object(View, 'print_result', MockView.print_result)
+#     mocker.patch.object(Operators, 'addition', MockOperators.addition)
+#     mocker.patch.object(Operators, 'substraction', MockOperators.substraction)
+#     mocker.patch.object(Operators, 'multiplication', MockOperators.multiplication)
+#     mocker.patch.object(Operators, 'division', MockOperators.division)
 
 
-def test_run_add(mocker):
+def test_run_add(setup_mock):
     sut = Controller()
     MockView.user_input_database = ("1", "100+40", "5")
     MockOperators.add_output_database = [140]
-
-    setup_mock(mocker)
 
     sut.run()
 
     assert len(MockView.print_result_call) == 1
     assert MockView.print_result_call[0] == ("100+40", 140)
 
-def test_run_add_bis(mocker):
+def test_run_add_bis(setup_mock):
     sut = Controller()
     MockView.user_input_database = ("1", "100+40+50","5")
     MockOperators.add_output_database = [190]
-    
-    setup_mock(mocker)
 
     sut.run()
 
     assert len(MockView.print_result_call) == 1
     assert MockView.print_result_call[0] == ("100+40+50", 190)
 
-def test_run_add_bis_another(mocker):
+def test_run_add_bis_another(setup_mock):
     sut = Controller()
     MockView.user_input_database = ("1", "100+40+50", "1", "1+2+3+4+5", "5")
     MockOperators.add_output_database = [190, 15]
-    
-    setup_mock(mocker)
 
     sut.run()
 
@@ -127,24 +143,20 @@ def test_run_add_bis_another(mocker):
     assert MockView.print_result_call[0] == ("100+40+50", 190)
     assert MockView.print_result_call[1] == ("1+2+3+4+5", 15)
 
-def test_run_sub(mocker):
+def test_run_sub(setup_mock):
     sut = Controller()
     MockView.user_input_database = ("2", "100-60", "5")
     MockOperators.sub_output_database = [40]
-
-    setup_mock(mocker)
 
     sut.run()
     
     assert len(MockView.print_result_call) == 1
     assert MockView.print_result_call[0] == ("100-60", 40.0)
 
-def test_run_multi(mocker):
+def test_run_multi(setup_mock):
     sut = Controller()
     MockView.user_input_database = ("3", "2*3","3", "4*6", "5")
     MockOperators.multi_output_database  = [6, 24]
-    
-    setup_mock(mocker)
 
     sut.run()
     
@@ -152,12 +164,10 @@ def test_run_multi(mocker):
     assert MockView.print_result_call[0] == ("2*3", 6) 
     assert MockView.print_result_call[1] == ("4*6", 24)
 
-def test_run_div(mocker):
+def test_run_div(setup_mock):
     sut = Controller()
     MockView.user_input_database = ("4", "6.0 / 2.0", "4", "6.0 / 0.0", "5")
     MockOperators.div_output_database  = [3.0, None]
-    
-    setup_mock(mocker)
 
     sut.run()
     
@@ -165,13 +175,11 @@ def test_run_div(mocker):
     assert MockView.print_result_call[0] == ("6.0 / 2.0", 3.0)
     assert MockView.print_result_call[1] == ("6.0 / 0.0", None)
 
-def test_run_allfunctions(mocker):
+def test_run_allfunctions(setup_mock):
     sut = Controller()
     MockView.user_input_database = ("3", "2*3","3", "4*6","4", "6.0 / 2.0", "4", "6.0 / 0.0", "5")
     MockOperators.multi_output_database = [6, 24]
     MockOperators.div_output_database = [3.0, None]
-    
-    setup_mock(mocker)
 
     sut.run()
     
